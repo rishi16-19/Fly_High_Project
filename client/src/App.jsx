e49fcffd8fd32b8a72050110225a91d03ffb1393
AshutoshRath1612 <ashutosh.rath1613@ibm.com>
2024-03-05 15:36:48 +0530

    added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dummyData = [
+    {flightCode:"ABC"  , sourceCode:"BBSR"  ,destCode:"CCU" ,  flightName:"Air Asia"  , flightDate:1 ,sourceTime: "20:20" ,destTime:"21:30" , price: 76372 },
+    {flightCode:"BCD"  , sourceCode:"CCU"  ,destCode:"DEL" ,  flightName:"Indigo"  , flightDate:3 ,sourceTime:"17:35" ,destTime:"15:00" , price: 650 },
+    {flightCode:"CDE"  , sourceCode:"DEL"  ,destCode:"BBSR" ,  flightName:"SpiceJet"  , flightDate:2 ,sourceTime:"16:25" ,destTime:"20:30" , price: 5 },
+    {flightCode:"DEF"  , sourceCode:"BBI"  ,destCode:"MUM" ,  flightName:"Air India"  , flightDate:2 ,sourceTime:"10:40" ,destTime:"21:10" , price: 5000 },
+    {flightCode:"EFG"  , sourceCode:"MUM"  ,destCode:"BBI" ,  flightName:"Air Asia"  , flightDate:3 ,sourceTime:"00:00" ,destTime:"4:50" , price: 500 }
+  ]
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
