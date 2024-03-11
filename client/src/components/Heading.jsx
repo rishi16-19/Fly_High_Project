a8222940b933d1644966dedbe85fe06769f3fbf9
RISHIKESH MISHRA <78504153+rishi16-19@users.noreply.github.com>
2024-03-11 20:12:27 +0530

    Delete client directory

--- client/src/components/Heading.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/prop-types */
-import React from 'react';
-
-const Heading = ({ level = 1, children }) => {
-  const HeadingTag = `h${Math.min(Math.max(level, 1), 6)}`;
-  return <HeadingTag>{children}</HeadingTag>;
-};
-
-export default Heading;
